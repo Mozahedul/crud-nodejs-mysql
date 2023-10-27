28a65f3e67dac46e6e4bf11769530132f1e679d4
Mozahedul <mozahed001@gmail.com>
2023-10-27 23:16:29 +0600

    https://www.freesqldatabase.com/ added

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-// Loading require modules 
-const express = require('express'),
-    mysql = require('mysql'),
-    bodyParser = require('body-parser'),
-    dateFormat = require('dateformat'),
-    path = require('path'),
-    cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    flash = require('connect-flash');
-const { start } = require('repl');
+// Loading require modules
+const express = require("express"),
+  mysql = require("mysql"),
+  bodyParser = require("body-parser"),
+  dateFormat = require("dateformat"),
+  path = require("path"),
+  cookieParser = require("cookie-parser"),
+  session = require("express-session"),
+  flash = require("connect-flash");
+const { start } = require("repl");
 
-// Create an express app 
+// Create an express app
 const app = express();
 
 // Define cookie parser
 app.use(cookieParser());
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, maxAge: 60000 }
-}));
+    cookie: { secure: false, maxAge: 60000 },
+  })
+);
 app.use(flash());
 
 // Parse the form
@@ -29,18 +31,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var now = new Date();
 
 // Define template engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Configure public Directory
-const publicDirectroy = path.join(__dirname, '/public');
+const publicDirectroy = path.join(__dirname, "/public");
 app.use(express.static(publicDirectroy));
 
 // Create a mysql connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'crud_mysql'
+  host: "sql12.freesqldatabase.com",
+  user: "sql12657125",
+  password: "ukhE93N3M8",
+  database: "sql12657125",
 });
 
 // Establish a mysql connection
@@ -48,123 +50,114 @@ db.connect();
 
 // Query from database
 // Create an express server for view tha data from a database
-app.get('/', (req, res) => {
-    db.query('select * from e_event order by start_date desc', (err, results) => {
-        res.render('pages/index', {
-            title: 'Home Page',
-            items: results,
-            serverSuccess: req.flash('server-success'),
-        });
+app.get("/", (req, res) => {
+  db.query("select * from e_event order by start_date desc", (err, results) => {
+    res.render("pages/index", {
+      title: "Home Page",
+      items: results,
+      serverSuccess: req.flash("server-success"),
     });
-
+  });
 });
 
 // Create a new event
 // Only load the form
-app.get('/event/add', (req, res) => {
-
-    res.render('pages/add-event', {
-        title: 'Add Event Page',
-        items: '',
-        serverError: req.flash('server-error')
-    });
+app.get("/event/add", (req, res) => {
+  res.render("pages/add-event", {
+    title: "Add Event Page",
+    items: "",
+    serverError: req.flash("server-error"),
+  });
 });
 
 // Send or insert the data into Database
-app.post('/event/add', (req, res) => {
-    const { name, start_date, end_date, e_desc, e_location } = req.body;
-    const obj = {
-        name: name,
-        start_date: dateFormat(start_date, "yyyy-mm-dd"),
-        end_date: dateFormat(end_date, "yyyy-mm-dd"),
-        e_desc: e_desc,
-        e_location: e_location
+app.post("/event/add", (req, res) => {
+  const { name, start_date, end_date, e_desc, e_location } = req.body;
+  const obj = {
+    name: name,
+    start_date: dateFormat(start_date, "yyyy-mm-dd"),
+    end_date: dateFormat(end_date, "yyyy-mm-dd"),
+    e_desc: e_desc,
+    e_location: e_location,
+  };
+  db.query("insert into e_event set ?", obj, (err, results) => {
+    if (err) {
+      req.flash("server-error", err.message);
+      res.redirect("/event/add");
+    } else {
+      req.flash("server-success", "Inserted successfully");
+      res.redirect("/");
     }
-    db.query('insert into e_event set ?', obj, (err, results) => {
-
-        if (err) {
-            req.flash('server-error', err.message);
-            res.redirect('/event/add');
-        } else {
-            req.flash('server-success', 'Inserted successfully');
-            res.redirect('/');
-        }
-
-
-    });
+  });
 });
 
 // Edit event ## View data inside the form
-app.get('/event/edit/:id', (req, res) => {
-    let id = req.params.id;
-    // Query the data form database
-    db.query("select * from e_event where id = ?", `${id}`, (err, results) => {
-        var string = JSON.stringify(results);
-        var qResults = JSON.parse(string);
+app.get("/event/edit/:id", (req, res) => {
+  let id = req.params.id;
+  // Query the data form database
+  db.query("select * from e_event where id = ?", `${id}`, (err, results) => {
+    var string = JSON.stringify(results);
+    var qResults = JSON.parse(string);
 
-        qResults[0].start_date = dateFormat(qResults[0].start_date, 'yyyy-mm-dd');
-        qResults[0].end_date = dateFormat(qResults[0].end_date, 'yyyy-mm-dd');
+    qResults[0].start_date = dateFormat(qResults[0].start_date, "yyyy-mm-dd");
+    qResults[0].end_date = dateFormat(qResults[0].end_date, "yyyy-mm-dd");
 
-        res.render('pages/edit-event', {
-            title: 'Edit event page',
-            items: qResults,
-        });
+    res.render("pages/edit-event", {
+      title: "Edit event page",
+      items: qResults,
     });
+  });
 });
 
 // Update event ## Update database
-app.post('/event/update/:id', (req, res) => {
-    var { name, start_date, end_date, e_desc, e_location } = req.body;
-    var id = req.params.id;
-    var sql = `UPDATE e_event SET 
+app.post("/event/update/:id", (req, res) => {
+  var { name, start_date, end_date, e_desc, e_location } = req.body;
+  var id = req.params.id;
+  var sql = `UPDATE e_event SET 
     name = "${name}", 
     start_date = "${start_date}",
     end_date = "${end_date}",
     e_desc = "${e_desc}",
     e_location = "${e_location}"
-    WHERE id = ${id}`
-    db.query(sql, (err, results) => {
-        // console.log(results);
-        if (!err) {
-            req.flash('server-success', `ID number: ${id}! Updated Successfully`);
-            res.redirect('/')
-        } else {
-            req.flash('server-error', err.message);
-            res.redirect('/event/edit');
-        }
-    });
-
+    WHERE id = ${id}`;
+  db.query(sql, (err, results) => {
+    // console.log(results);
+    if (!err) {
+      req.flash("server-success", `ID number: ${id}! Updated Successfully`);
+      res.redirect("/");
+    } else {
+      req.flash("server-error", err.message);
+      res.redirect("/event/edit");
+    }
+  });
 });
 
 // Delete a row from a database
-app.get('/event/delete/:id', (req, res) => {
-    var id = req.params.id;
-    var sql = `DELETE FROM e_event WHERE id = ${id}`;
-    db.query(sql, (err, results) => {
-        if (!err) {
-            req.flash('server-success', `ID Number: ${id}! Deleted successfully`);
-            res.redirect('/')
-        } else {
-            req.flash('server-error', err.message);
-            res.redirect('/');
-        }
-    })
-});
-
-// Handle 404 page 
-app.use(function (req, res, next) {
-    res.status(404).render('pages/404', {
-        title: '404 Error!'
-    });
-});
-
-app.listen(3232, (err) => {
-    if (err) {
-        console.log(err);
+app.get("/event/delete/:id", (req, res) => {
+  var id = req.params.id;
+  var sql = `DELETE FROM e_event WHERE id = ${id}`;
+  db.query(sql, (err, results) => {
+    if (!err) {
+      req.flash("server-success", `ID Number: ${id}! Deleted successfully`);
+      res.redirect("/");
     } else {
-        console.log('Server connected');
+      req.flash("server-error", err.message);
+      res.redirect("/");
     }
-})
+  });
+});
 
+// Handle 404 page
+app.use(function (req, res, next) {
+  res.status(404).render("pages/404", {
+    title: "404 Error!",
+  });
+});
 
-
+app.listen(3232, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server connected");
+  }
+});
